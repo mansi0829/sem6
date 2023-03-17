03f1e15cf861f4ce711890cdee989ab803d5884d
mansi0829 <mansigundre1@gmail.com>
2023-03-17 09:43:48 +0530

    spring added

---
+++ WTL/A5/a5/src/main/java/com/a5/a5/A5Application.java
@@ -0,0 +1,13 @@
+package com.a5.a5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A5Application.class, args);
+	}
+
+}
